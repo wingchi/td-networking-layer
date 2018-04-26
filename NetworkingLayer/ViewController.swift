7d69e0ef0fd9b75fb0a881f1cca6f872468edb83
S. Wingchi Wong <stephenwong@intrepid.io>
2018-04-26 15:51:48 -0400

    Add base iOS project

---
+++ NetworkingLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkingLayer
+//
+//  Created by wingchi on 4/26/18.
+//  Copyright © 2018 The Test and the Driven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
